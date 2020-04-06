c97cc18048b40feba570ad7dc95a54da5e8795ac
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-06 18:37:11 +1000

    bugfix-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/projects'
+  get 'welcome/blog'
+  get 'welcome/contact'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
