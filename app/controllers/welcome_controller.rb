2f1fcb3c4ea98f3c0da8a95be048159dc323088a
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-07 08:58:53 +1000

    bugfix-28

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+    @page_title = "Home"
   end
   def about
+    @page_title = "About"
   end
   def projects
+    @page_title = "Projects"
   end
   def blog
+    @page_title = "Blog"
   end
   def contact
+    @page_title = "Contact"
   end
 end
