50c303e067470c6da5cc7fe42566aac7bbfb406c
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-07 09:37:58 +1000

    bugfix-29

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,21 @@
 class WelcomeController < ApplicationController
+  def initialize
+    @flexible_footer = ""
+  end
   def index
     @page_title = "Home"
   end
   def about
     @page_title = "About"
+    @flexible_footer = "flexible-footer"
   end
   def projects
     @page_title = "Projects"
+    @flexible_footer = "flexible-footer"
   end
   def blog
     @page_title = "Blog"
+    @flexible_footer = "flexible-footer"
   end
   def contact
     @page_title = "Contact"
