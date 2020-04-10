95e2bb75554d41aa9631a4ae8c793a046aa4910f
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-10 21:18:41 +1000

    bugfix-47

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,6 @@ class ProjectsController < ApplicationController
     @flexible_footer = "flexible-footer"
     @cur_page = Hash.new("")
     @cur_page[:project] = "cur-category"
-    render plain: @projects[0].title
   end
 
   # GET /projects/1
