21d90fdb0cc90704eda0cc411679436fd89261fc
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-11 16:01:50 +1000

    bugfix-62

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :blogs
+  
   get "/home", to: "home#index"
   get "/about", to: "about#index"
-  get "/blogs", to: "blogs#index"
-  get "/projects", to: "projects#index"
   get "/contact", to: "contact#index"
   
   resources :projects
+  resources :blogs
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
