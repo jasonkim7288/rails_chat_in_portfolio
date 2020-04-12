3b79242f41860d6299fa1b9106799442894dfa75
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-12 12:04:08 +1000

    gitignore Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200410043940) do
+ActiveRecord::Schema.define(version: 20200411051015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "photo_path"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "author_id"
+    t.date "create_date"
+    t.string "pic_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_blogs_on_author_id"
+  end
+
+  create_table "paragraphs", force: :cascade do |t|
+    t.integer "order"
+    t.text "content"
+    t.bigint "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_paragraphs_on_blog_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -28,4 +55,6 @@ ActiveRecord::Schema.define(version: 20200410043940) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "authors"
+  add_foreign_key "paragraphs", "blogs"
 end
