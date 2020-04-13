6113c700dd03bb69ce49d086629a50434726d923
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-14 00:30:19 +1000

    add channel and job

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :blogs
 
   root 'home#index'
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
