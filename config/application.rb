e9d607727954df7d78c1d7b590c0802d885f55ab
jasonkim7288 <jason.kim7288@gmail.com>
2020-04-14 22:10:13 +1000

    change config of Devise for heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Myapp
     config.load_defaults 5.1
 
     config.time_zone = 'Brisbane'
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
