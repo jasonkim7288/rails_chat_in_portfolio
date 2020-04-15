f55dc8268c85f42e80454cfdfe9ba4d5be104eaa
jasonkim7288 <jason.kim7288@gmail.com>
2020-04-15 21:00:29 +1000

    change the view path to pages folder

--- app/controllers/about_controller.rb
+++
@@ -1,8 +0,0 @@
-class AboutController < ApplicationController
-  def index
-    @page_title = "About"
-    @flexible_footer = "flexible-footer"
-    @cur_page = Hash.new("")
-    @cur_page[:about] = "cur-category"
-  end
-end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @page_title = "Home"
+    @flexible_footer = ""
+    @cur_page = Hash.new("")
+    @cur_page[:home] = "cur-category"
   end
 
   def about
+    @page_title = "About"
+    @flexible_footer = "flexible-footer"
+    @cur_page = Hash.new("")
+    @cur_page[:about] = "cur-category"
   end
 
   def contact
+    @page_title = "Contact"
+    @flexible_footer = ""
+    @cur_page = Hash.new("")
+    @cur_page[:contact] = "cur-category"
   end
 end
