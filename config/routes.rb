81e3f9cb4657ccf6996f56d632092ba74a663df0
jasonkim7288 <jason.kim7288@gmail.com>
2020-04-15 20:55:46 +1000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
   devise_for :users
-  get "/home", to: "home#index"
-  get "/about", to: "about#index"
-  get "/contact", to: "contact#index"
+  get "/home", to: "pages#home"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
   get "/chat", to: "rooms#show"
   # get 'user_root', to: redirect('/chat'), as: :user_root
   
   resources :projects
   resources :blogs
 
-  root 'home#index'
+  root 'pages#home'
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
