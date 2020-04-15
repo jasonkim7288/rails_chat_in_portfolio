5a6741e11d301ddb7284717329d587f71da4e80c
jasonkim7288 <jason.kim7288@gmail.com>
2020-04-15 20:52:24 +1000

    generate Pags controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   devise_for :users
   get "/home", to: "home#index"
   get "/about", to: "about#index"
